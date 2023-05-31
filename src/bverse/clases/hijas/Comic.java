5da8ee492098ee6f4b3003f978a992a729725e37
inaDAGod <danialee14@gmail.com>
2023-05-31 16:05:22 -0400

    cambio de tipo de publicacion

--- src/bverse/clases/hijas/Comic.java
+++ src/bverse/clases/hijas/Comic.java
@@ -1,12 +1,13 @@
 package bverse.clases.hijas;
 
+import bverse.categorizaciones.Tipo;
 import bverse.clases.madres.Publicacion;
 
 public class Comic extends Publicacion{
 
 	public Comic(String iSBM, String titulo, String precio, String portadaUrl, String calpromedio, String descripcion,
-			Autor autor, int paginas, int estado, int genero, int tipo, String calificacion, String resenia) {
-		super(iSBM, titulo, precio, portadaUrl, calpromedio, descripcion, autor, paginas, estado, genero, tipo, calificacion,
+			Autor autor, int paginas, int estado, int genero, String calificacion, String resenia) {
+		super(iSBM, titulo, precio, portadaUrl, calpromedio, descripcion, autor, paginas, estado, genero, Tipo.comic, calificacion,
 				resenia);
 		// TODO Auto-generated constructor stub
 	}
--- src/bverse/clases/hijas/NovelaGrafica.java
+++ src/bverse/clases/hijas/NovelaGrafica.java
@@ -1,13 +1,14 @@
 package bverse.clases.hijas;
 
+import bverse.categorizaciones.*;
 import bverse.clases.madres.*;
 
 public class NovelaGrafica extends Publicacion{
 
 	public NovelaGrafica(String iSBM, String titulo, String precio, String portadaUrl, String calpromedio,
-			String descripcion, Autor autor, int paginas, int estado, int genero, int tipo, String calificacion,
+			String descripcion, Autor autor, int paginas, int estado, int genero, String calificacion,
 			String resenia) {
-		super(iSBM, titulo, precio, portadaUrl, calpromedio, descripcion, autor, paginas, estado, genero, tipo, calificacion,
+		super(iSBM, titulo, precio, portadaUrl, calpromedio, descripcion, autor, paginas, estado, genero, Tipo.novela_grafica, calificacion,
 				resenia);
 		// TODO Auto-generated constructor stub
 	}
