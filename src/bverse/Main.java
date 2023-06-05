24918b63ddbce866ff1a1385e685e81b002364c4
inaDAGod <danialee14@gmail.com>
2023-06-05 13:34:52 -0400

    conexion base de datos

--- src/bverse/Main.java
+++ src/bverse/Main.java
@@ -1,10 +1,12 @@
 package bverse;
-
+import bverse.baseDatos.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("hola");
+		Conexion c = new Conexion();
+		c.getConexionPostgres();
 		
 	}
 
